b4096446ded957b525216a2d93390098823fbd9e
Matthieu Riegler <kyro38@gmail.com>
2024-02-06 00:19:23 +0100

    Repro: No export throws

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
+import { Component, input } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+@Component({
+  selector: 'component-parent',
+  standalone: true,
+  template: `{{ val() }} `,
+})
+class Child {
+  val = input('');
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [Child],
+  template: `
+    <h1>Hello, {{ title }}</h1>
+    <component-parent [val]="title" />
+  `,
+})
+export class AppComponent {
+  title = 'signals';
+}
+
+bootstrapApplication(AppComponent);
